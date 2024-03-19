246c73fbdbc6409ac990ad338ddaa90f512f6249
SankritaPatel <sankrita6960@outlook.com>
2024-03-19 08:38:25 +0530

    Model trained

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ import sys
 from src.StudentPerformanceIndicator.logger import logging
 from src.StudentPerformanceIndicator.exception import CustomException
 from src.StudentPerformanceIndicator.components.data_ingestion import DataIngestion
-from src.StudentPerformanceIndicator.components.data_ingestion import DataIngestionConfig
-from src.StudentPerformanceIndicator.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.StudentPerformanceIndicator.components.data_transformation import DataTransformation
+from src.StudentPerformanceIndicator.components.model_trainer import ModelTrainer
 
 if __name__=="__main__":
     logging.info("The execution has started...")
 
     try:
-        # data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
-        #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        
+        model_trainer = ModelTrainer()
+        r2Score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+        print(r2Score)
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
